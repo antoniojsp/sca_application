fa11c838c1cc7b293b2855bda26d5a298a7e887d
antonio Silva <antonios@uoregon.edu>
2022-07-09 06:17:33 -0700

    last good

--- form.py
+++ form.py
@@ -9,10 +9,13 @@ class Application(FlaskForm):
     full_name = StringField(label='Full Name', validators=[DataRequired()],
                       render_kw={"class": "form-control p-2s",
                                  'autocomplete': "off"})
-    today_date = DateTimeField(label="Today's Date", format='%m/%d/%y', validators=[DataRequired()])
+    today_date = DateTimeField(label="Today's Date", format='%m/%d/%y', validators=[DataRequired()],render_kw={"class": "form-control p-2s",
+                                 'autocomplete': "off"})
 
-    initials1 = StringField(label="Initials", validators=[DataRequired()])
-    initials2 = StringField(label="Initials", validators=[DataRequired()])
+    initials1 = StringField(label="Initials", validators=[DataRequired()],render_kw={"class": "form-control p-2s",
+                                 'autocomplete': "off"})
+    initials2 = StringField(label="Initials", validators=[DataRequired()],render_kw={"class": "form-control p-2s",
+                                 'autocomplete': "off"})
     initials3 = StringField(label="Initials", validators=[DataRequired()])
     initials4 = StringField(label="Initials", validators=[DataRequired()])
     initials5 = StringField(label="Initials", validators=[DataRequired()])
@@ -27,9 +30,9 @@ class Application(FlaskForm):
 
 
     full_legal_name = StringField(label="Full Legal Name", validators=[DataRequired()])
-    preferred_name = StringField(label="Prefered Name", validators=[DataRequired()])
+    preferred_name = StringField(label="Prefered Name")
     pronouns = StringField(label="Pronouns", validators=[DataRequired()])
-    dob =  DateTimeField(label="Date of Birth")
+    dob =  DateTimeField(label="Date of Birth", validators=[DataRequired()])
     email = StringField(label="Email",  validators=[DataRequired(), Email()])
     phone = IntegerField('Phone', validators=[DataRequired()])
     permanent_address =StringField(label="Address", validators=[DataRequired()])
@@ -37,22 +40,25 @@ class Application(FlaskForm):
 
 
     term_move_in = SelectField(label="What term you plan to move in?", choices=["Fall", "Winter", "Spring", "Summer"])
-    year = IntegerField(label="Year")
+    year = IntegerField(label="Year", validators=[DataRequired()])
 
+    #Ajax
     was_boarder = BooleanField("Have you ever been a resident or boarder of the SCA?")
     years_boarder = IntegerField(label="Year")
 
     denied_membership = BooleanField("Have you ever been denied membership with the SCA? (Answering 'yes’ to this question does /"
                                      "not necessarily disqualify you from membership.)")
 
-    member_terminated = StringField(label="Have you ever had your membership or tenancy terminated by the SCA and/or another /" \
+    #Ajax
+    member_terminated = BooleanField(label="Have you ever had your membership or tenancy terminated by the SCA and/or another /" \
                         "housing organization (Answering ‘yes’ to this question does not necessarily disqualify " \
                         "you from membership.)")
     member_terminated_explanation = StringField(label="If yes, explain")
 
-    type_student = SelectField(label="Type of Student", choices=["Undergrad", "Graduate", "Post-Baccalaureate"])
-    school_year = IntegerField(label="School Year", validators=[DataRequired()])
-    credits = IntegerField(label="Credits", validators=[DataRequired()])
+    #AJax
+    type_student = SelectField(label="Type of Student", choices=["Non Student", "Undergrad", "Graduate", "Post-Baccalaureate"])
+    school_year = IntegerField(label="School Year")
+    credits = IntegerField(label="Credits")
     major = StringField(label="Major")
 
     house_preference = SelectField(label="Type of Student", choices=["JS", "Lorax", "Campbell Club"])
@@ -60,11 +66,14 @@ class Application(FlaskForm):
                                     "houses, please let us know:")
     waitlist = BooleanField("Are you willing to be placed on a waiting list for an open housing spot?")
     allergies = StringField("Allergies?")
-    how_long =  StringField(label="How long do you expect to live in these houses? If you don’t know that’s okay.")
+    how_long = StringField(label="How long do you expect to live in these houses? If you don’t know /"
+                                 "that’s okay.", validators=[DataRequired()])
+    #Ajax
     pet = BooleanField("Do you have a pet you’d like to bring?")
     pet_description = StringField(label="Pet Description: ")
     pet_needs = StringField(label="What are your pet’s needs (outdoor/indoor/other) and how they tend to interact with other" +
                                   "animals?: ")
+
     heer_about_us = SelectField(label="How did you hear about us?", choices=["Radio", "Fb", "Craiglist", "Friend"])
 
 
