77b16372fccdd732e28f0f30f6763c3ee6b8260e
Antonio Silva Paucar <antonios@uoregon.edu>
2022-07-23 22:06:42 -0700

    Loop and concat in jinja working.

--- form.py
+++ form.py
@@ -7,9 +7,9 @@ from wtforms.validators import DataRequired, Length, Email
 
 
 class CoverForm(FlaskForm):
-    full_name = StringField(label='Full Name', validators=[DataRequired()],
+    full_name = StringField(validators=[DataRequired()],
                             render_kw={'class':"cover", 'autocomplete': "off", "style":"width: 400px;"})
-    today_date = DateField(label="Today's Date", format='%m/%d/%y',
+    today_date = DateField(format='%m/%d/%y',
                            render_kw={'autocomplete': "off", 'class': "cover"},
                            validators=[DataRequired()])
 
@@ -17,8 +17,8 @@ class CoverForm(FlaskForm):
 class AgreementForm(Form):
     initials = StringField(validators=[DataRequired()],
                            render_kw={'autocomplete': "off",
-                                      "maxlength":"4",
-                                      "style":"width: 60px;",
+                                      "maxlength": "4",
+                                      "style": "width: 60px;",
                                       "placeholder": "initials",
                                       'class': 'agreement'})
 
@@ -28,87 +28,63 @@ class ChecklistForm(Form):
 
 
 class PersonalInformationForm(Form):
-    full_legal_name = StringField(label="Full Legal Name",
-                                  validators=[DataRequired()],
+    full_legal_name = StringField(validators=[DataRequired()],
                                   render_kw={"style":"width: 400px;"})
-    preferred_name = StringField(label="Preferred Name")
-    pronouns = StringField(label="Select your preferred pronoun or add one. ")
-    dob = DateField(label="Date of Birth",
-                    format='%m/%d/%y',
+    preferred_name = StringField()
+    pronouns = StringField()
+    dob = DateField(format='%m/%d/%y',
                     validators=[DataRequired()],
                     render_kw={'autocomplete': "off"})
-    email = StringField(label="Email",
-                        validators=[DataRequired()],
+    email = StringField(validators=[DataRequired(), Email("This field requires a valid email address")],
                         render_kw={"placeholder": "example@mail.com",
                                    "type": "email"})
-    phone = IntegerField(label='Phone',
-                         validators=[DataRequired()])
-    permanent_address = StringField(label="Address",
-                                    validators=[DataRequired()],
+    phone = IntegerField(validators=[DataRequired()])
+    permanent_address = StringField(validators=[DataRequired()],
                                     render_kw={"style": "width: 500px;"})
-    current_address = StringField(label="Current Address",
-                                  validators=[DataRequired()],
+    current_address = StringField(validators=[DataRequired()],
                                   render_kw={"style": "width: 500px;"})
 
 
     my_choices = [('1', 'Fall'), ('2', 'Spring'), ('3', 'Summer'), ('4', "Winter")]
-    move_in_term = SelectMultipleField(label="Check the term you wish to move in. "
-                                             "If you’re flexible, mark multiple terms "
-                                             "(Use command to select multiple). "
-                                             "If you intend to move in during the middle of a term, please let us know",
-                                       choices=my_choices,
+    move_in_term = SelectMultipleField(choices=my_choices,
                                        validators=[DataRequired()])
 
-    move_in_year= IntegerField(label="Year",
-                               validators= [DataRequired()])
+    move_in_year= IntegerField(validators= [DataRequired()])
 
-    was_boarder = BooleanField("Have you ever been a resident or boarder of the SCA? Yes",
-                               render_kw={'onclick':"showStuff('was_boarder_checked')"})
-    years_boarder = IntegerField(label="In what year", render_kw={"min": "1920", "max": "2022"})
+    # JS Magic
+    was_boarder = BooleanField(render_kw={'onclick': "showStuff('was_boarder_checked')"})
+    years_boarder = IntegerField(render_kw={"min": "1920", "max": "2022"})
 
-    denied_membership = BooleanField("Have you ever been denied membership with the SCA? (Checking this option does"\
-                                     "not necessarily disqualify you from membership.)")
+    denied_membership = BooleanField()
 
-    member_terminated = BooleanField(label="Have you ever had your membership or tenancy terminated "
-                                           "by the SCA and/or another housing organization (Checking "
-                                           "this question does not necessarily disqualify you "
-                                           "from membership.) Yes",
-                                     render_kw={"onclick": "showStuff('was_boarder_terminated')"})
-    member_terminated_explanation = TextAreaField(label="If yes, explain", render_kw={"rows": "12", "cols": "150"})
+    member_terminated = BooleanField(render_kw={"onclick": "showStuff('was_boarder_terminated')"})
+    member_terminated_explanation = TextAreaField(render_kw={"rows": "12", "cols": "150"})
 
-
-    type_student = SelectField(label="Type of Student",
-                               choices=["Non Student", "Undergrad", "Graduate", "Post-Baccalaureate"],
-                               render_kw={"onchange": "hide_if_selected(this.value, 'is_student', 'Non Student')"})
+    type_student = SelectField(choices=["Non Student", "Undergrad", "Graduate", "Post-Baccalaureate"], render_kw={"onchange": "hide_if_selected(this.value, 'is_student', 'Non Student')"})
     which_school = StringField(render_kw={"style":"width: 400px;"})
-    school_year = IntegerField(label="School Year")
-    credits = IntegerField(label="Credits")
-    major = StringField(label="Major")
+    school_year = IntegerField()
+    credits = IntegerField()
+    major = StringField()
 
     house_preference = SelectField(choices=["None", "JS", "Lorax", "Campbell Club"])
     room_preference = SelectField(choices=["None", "Single", "Small Single", "Double"])
 
-    house_no_available = BooleanField("If your choice of room or household is not available, would you be willing to be assigned to "
-                                      "another house and/or room?")
-    if_not_available = BooleanField("If you have a strong preference for one house or will only consider living in one or two of the"
-                                    "houses, please let us know:")
-    waitlist = BooleanField("Are you willing to be placed on a waiting list for an open housing spot?")
-    allergies = StringField("Allergies?")
-    how_long = StringField(label="How long do you expect to live in these houses? If you don’t know /"
-                                 "that’s okay.", validators=[DataRequired()])
-    #Ajax
-    pet = BooleanField("Do you have a pet you’d like to bring?",
-                       render_kw={'onclick':"showStuff('has_pet')"})
-    pet_description = StringField(label="Pet Description: ")
-    pet_needs = StringField(label="What are your pet’s needs (outdoor/indoor/other) and how they tend to interact with other" +
-                                  "animals?: ")
-    hear_about_us = SelectMultipleField(label="How did you hear about us?", choices=["Radio", "Fb", "Craiglist", "Friend", "Other"],
-                                    render_kw={"onclick": "show_if_selected(this.value, 'other_hear', 'Other')"})
-    if_other_about_us = StringField(label="If other, please, specify.")
+    house_no_available = BooleanField()
+    if_not_available = BooleanField()
+    waitlist = BooleanField()
+    allergies = StringField()
+    how_long = StringField(validators=[DataRequired()])
+    #JS Magic
+    pet = BooleanField(render_kw={'onclick': "showStuff('has_pet')"})
+    pet_description = StringField()
+    pet_needs = StringField()
+    hear_about_us = SelectMultipleField(choices=["Radio", "Fb", "Craiglist", "Friend", "Other"],
+                                        render_kw={"onclick": "show_if_selected(this.value, 'other_hear', 'Other')"})
+    if_other_about_us = StringField()
 
 
 class ShortEssayForm(Form):
-    question = TextAreaField(render_kw={"rows":"12", "cols":"150"})
+    question = TextAreaField(render_kw={"rows": "12", "cols": "150"})
 
 
 class AutobiographicalForm(Form):
@@ -116,14 +92,14 @@ class AutobiographicalForm(Form):
 
 
 class Range(Form):
-    range = IntegerRangeField(label= "range", render_kw={"min": 0,
-                                                         "max": 10,
-                                                         "step": 1,
-                                                         "style": "width:500px;"})
+    range = IntegerRangeField(label="range", render_kw={"min": 0,
+                                                        "max": 10,
+                                                        "step": 1,
+                                                        "style": "width:500px;"})
     input = IntegerField(render_kw={"type": "number",
-                                     "value": "5",
-                                     "min": "0",
-                                     "max": "10"})
+                                    "value": "5",
+                                    "min": "0",
+                                    "max": "10"})
     comment = StringField("Comments:", render_kw={"placeholder": "Comments"})
 
 
